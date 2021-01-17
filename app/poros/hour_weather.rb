e0a0b10acdab567fbe9e367ec6d88f7cc199e599
Jake Heft <jakeheft@gmail.com>
2021-01-17 11:59:05 -0700

    Add hour weather poro with initialize, get time, get directions methods

---
+++ app/poros/hour_weather.rb
@@ -0,0 +1,22 @@
+class HourWeather
+	attr_reader :time, :temp, :wind_speed, :wind_direction, :conditions, :icon
+
+	def initialize(data)
+		@time = HourWeather.get_time(data[:dt])
+		@temp = data[:temp]
+		@wind_speed = "#{data[:wind_speed]} mph"
+		@wind_direction = HourWeather.get_direction(data[:wind_deg])
+		@conditions = data[:weather][0][:description]
+		@icon = data[:weather][0][:icon]
+	end
+
+	def self.get_time(date_time)
+		Time.at(date_time).to_s[11..15]
+	end
+
+	def self.get_direction(wind_deg)
+		directions = ['N','NNE','NE','ENE','E','ESE', 'SE', 'SSE','S','SSW','SW','WSW','W','WNW','NW','NNW', 'N']
+		converted_deg = (wind_deg/22.5 + 0.5).to_i
+		"from #{directions[converted_deg]}"
+	end
+end
