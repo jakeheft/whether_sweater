9f0fea8bb335aa5d539a9077a8346984e8347487
Jake Heft <jakeheft@gmail.com>
2021-01-17 13:43:00 -0700

    Add daily weather poro with initialize and get date methods

---
+++ app/poros/daily_weather.rb
@@ -0,0 +1,17 @@
+class DailyWeather
+	attr_reader :date, :sunrise, :sunset, :max_temp, :min_temp, :conditions, :icon
+
+	def initialize(data)
+		@date = DailyWeather.get_date(data[:dt])
+		@sunrise = Time.at(data[:sunrise]).to_s
+		@sunset = Time.at(data[:sunset]).to_s
+		@max_temp = data[:temp][:max]
+		@min_temp = data[:temp][:min]
+		@conditions = data[:weather][0][:description]
+		@icon = data[:weather][0][:icon]
+	end
+
+	def self.get_date(date_time)
+		Time.at(date_time).to_s[0..9]
+	end
+end
