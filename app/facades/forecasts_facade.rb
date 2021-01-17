7238f846e04bc97acd27688a72fca6df95b0b030
Jake Heft <jakeheft@gmail.com>
2021-01-17 14:05:39 -0700

    Add forecast poro creation with other objects

--- app/facades/forecasts_facade.rb
+++ app/facades/forecasts_facade.rb
@@ -10,12 +10,12 @@ class ForecastsFacade
 		### do we want to remove coordinates facade and call coordinates service from this method (like Ian suggested in video) - would need to call first, obviously
 		forecast_data = ForecastsService.fetch_forecast(lat_lng)
 		current_weather = CurrentWeather.new(forecast_data[:current])
-		require 'pry'; binding.pry
 		hourly_weather = forecast_data[:hourly][0..7].map do |hour_data|
-			HourWeather.new(hour_data)
+			HourlyWeather.new(hour_data)
 		end
 		daily_weather = forecast_data[:daily][0..4].map do |daily_data|
 			DailyWeather.new(daily_data)
 		end
+		Forecast.new(current_weather, hourly_weather, daily_weather)
 	end
 end
