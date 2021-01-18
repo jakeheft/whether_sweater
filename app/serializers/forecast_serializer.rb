a20e8bb8789906da5fad2fed6138df92760eed47
Jake Heft <jakeheft@gmail.com>
2021-01-18 09:27:54 -0700

    Update forecast serializer to correct syntax

--- app/serializers/forecast_serializer.rb
+++ app/serializers/forecast_serializer.rb
@@ -1,6 +1,5 @@
 class ForecastSerializer
 	include FastJsonapi::ObjectSerializer
-	set_id: nil
-	set_type: 'forecast'
-	attributes: :current_weather, :daily_weather, :hourly_weather
+	set_id { nil }
+	attributes :current_weather, :daily_weather, :hourly_weather
 end
