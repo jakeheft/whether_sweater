544795440b675a59ebef1958402edebd42423694
Jake Heft <jakeheft@gmail.com>
2021-01-18 15:14:53 -0700

    Add create user route

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,10 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       get '/forecast', to: 'forecasts#show'
+      
       get '/backgrounds', to: 'backgrounds#show'
+
+      post '/users', to: 'users#create'
     end
   end
 end
