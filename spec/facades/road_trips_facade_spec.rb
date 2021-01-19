ea711ef184818ece343233391d5fed4891505182
Jake Heft <jakeheft@gmail.com>
2021-01-19 14:08:36 -0700

    Add missing do block and end to test

--- spec/facades/road_trips_facade_spec.rb
+++ spec/facades/road_trips_facade_spec.rb
@@ -1,11 +1,12 @@
 require 'rails_helper'
 
 describe RoadTripsFacade do
-	it '.create_trip()'
+	it '.create_trip()' do
 		origin = 'Denver,CO'
 		destination = 'Austin,TX'
 		api_key = 'Sdfiodso*DfSD89sdf'
 
 		road_trip = RoadTripsFacade.create_trip(origin, destination, api_key)
 		expect(road_trip).to be_a(RoadTrip)
+	end
 end
