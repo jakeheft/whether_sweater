a2d3ef0028561ed5f4d1f31962b23664e7b4f498
Jake Heft <jakeheft@gmail.com>
2021-01-19 14:05:38 -0700

    Add test for create trip method of road trip facade

---
+++ spec/facades/road_trips_facade_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+
+describe RoadTripsFacade do
+	it '.create_trip()'
+		origin = 'Denver,CO'
+		destination = 'Austin,TX'
+		api_key = 'Sdfiodso*DfSD89sdf'
+
+		road_trip = RoadTripsFacade.create_trip(origin, destination, api_key)
+		expect(road_trip).to be_a(RoadTrip)
+end
