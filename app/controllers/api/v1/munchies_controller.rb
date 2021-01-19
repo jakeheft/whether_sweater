5f40ef05de4ac748f6a6bb3db19b46cbe4ec7373
Jake Heft <jakeheft@gmail.com>
2021-01-19 11:06:23 -0700

    Add food to show params

--- app/controllers/api/v1/munchies_controller.rb
+++ app/controllers/api/v1/munchies_controller.rb
@@ -1,5 +1,5 @@
 class Api::V1::MunchiesController < ApplicationController
 	def show
-		MunchiesFacade.get_munchies(params[:start], params[:end])
+		MunchiesFacade.get_munchies(params[:start], params[:end], params[:food])
 	end
 end
