214b39ea65d7e6af3df96d1459f1d6901b52ad02
Jake Heft <jakeheft@gmail.com>
2021-01-19 15:33:12 -0700

    Add comment for later

--- app/controllers/api/v1/road_trips_controller.rb
+++ app/controllers/api/v1/road_trips_controller.rb
@@ -1,5 +1,5 @@
 class Api::V1::RoadTripsController < ApplicationController
 	def create
-		road_trip = RoadTripsFacade.create_trip(params[:road_trip][:origin], params[:road_trip][:destination], params[:road_trip][:api_key])
+		road_trip = RoadTripsFacade.create_trip(params[:road_trip][:origin], params[:road_trip][:destination], params[:road_trip][:api_key]) ### remove api_key from arguments?
 	end
 end
