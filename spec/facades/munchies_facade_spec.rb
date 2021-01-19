52dac5bd265c36cd344bdc3b2481a7654cbec935
Jake Heft <jakeheft@gmail.com>
2021-01-19 11:06:00 -0700

    Change directions facade to munchies

--- spec/facades/munchies_facade_spec.rb
+++ spec/facades/munchies_facade_spec.rb
@@ -4,8 +4,9 @@ describe MunchiesFacade do
 	it '.get_destination_info' do
 		start = 'denver,co'
 		ending = 'pueblo,co'
+		food = 'chinese'
 
-		munchie = MunchiesFacade.get_munchies(start, ending)
+		munchie = MunchiesFacade.get_munchies(start, ending, food)
 
 		expect(munchie).to be_a(Munchie)
 	end
