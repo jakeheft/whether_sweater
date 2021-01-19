91d7461bd38e72ac73c9319f787761d427d05166
Jake Heft <jakeheft@gmail.com>
2021-01-19 15:53:27 -0700

    Refactor conn and parsed into their own methods

--- app/services/maps_service.rb
+++ app/services/maps_service.rb
@@ -1,20 +1,27 @@
 class MapsService
 	def self.fetch_coordinates(location)
-		conn = Faraday.new('https://www.mapquestapi.com')
 		response = conn.get('/geocoding/v1/address') do |req|
 			req.params[:key] = ENV['MAPQUEST_API_KEY']
 			req.params[:location] = location
 		end
-		JSON.parse(response.body, symbolize_names: true)
+		parsed(response)
 	end
 
 	def self.fetch_trip_info(origin, destination)
-		conn = Faraday.new('https://www.mapquestapi.com')
 		response = conn.get('/directions/v2/route') do |req|
 			req.params[:key] = ENV['MAPQUEST_API_KEY']
 			req.params[:from] = origin
 			req.params[:to] = destination
 		end
+		parsed(response)
+	end
+
+	private
+	def self.conn
+		Faraday.new('https://www.mapquestapi.com')
+	end
+
+	def self.parsed(response)
 		JSON.parse(response.body, symbolize_names: true)
 	end
 end
