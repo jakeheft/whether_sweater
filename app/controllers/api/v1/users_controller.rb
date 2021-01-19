31cac656a6e859431e05916a4641f27d2f7bb234
Jake Heft <jakeheft@gmail.com>
2021-01-18 18:47:44 -0700

    Update create method to check for email in db

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,11 +1,16 @@
 class Api::V1::UsersController < ApplicationController
 	def create
+		# require 'pry'; binding.pry
 		new_user = User.new(
 			email: params[:user][:email],
 			password: params[:user][:password],
 			api_key: SecureRandom.uuid
 		)
-		new_user.save
-		render json: UsersSerializer.new(new_user), status: :created
+		if User.where(email: params[:user][:email])
+			render json: ErrorSerializer.duplicate_email, status: :conflict
+		else
+			new_user.save
+			render json: UsersSerializer.new(new_user), status: :created
+		end
 	end
 end
