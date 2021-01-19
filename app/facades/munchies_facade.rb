09c728881e175633a0737d53ba78309af02468de
Jake Heft <jakeheft@gmail.com>
2021-01-19 11:07:11 -0700

    Change directions facade to munchies

--- app/facades/munchies_facade.rb
+++ app/facades/munchies_facade.rb
@@ -1,8 +1,18 @@
 class MunchiesFacade
-	def self.get_munchies(start, ending)
+	def self.get_munchies(start, ending, food)
 		destination_info = DirectionsService.fetch_destination_info(start, ending)
 		destination = Destination.new(destination_info[:route][:formattedTime], destination_info[:route][:locations][1])
 		forecast = ForecastsFacade.get_forecasts(ending)
+		dest_time = time_at_destination(destination.travel_time)
+		restaurant_data = RestaurantsService.fetch_restaurant(ending, food, dest_time)
 		require 'pry'; binding.pry
+		restaurant = Restaurant.new(restaurant_data[:businesses][0])
+	end
+	
+	def self.time_at_destination(travel_time)
+		hrs = travel_time[0..1].to_i
+		mins = travel_time[9..10].to_i
+		time = Time.now + hrs.hours + mins.minutes
+		time.to_i
 	end
 end
