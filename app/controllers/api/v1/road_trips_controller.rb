51913cdfe7d2b19893e1a240223c50db2ed0ba38
Jake Heft <jakeheft@gmail.com>
2021-01-19 19:14:21 -0700

    Add render json to serializer

--- app/controllers/api/v1/road_trips_controller.rb
+++ app/controllers/api/v1/road_trips_controller.rb
@@ -1,5 +1,6 @@
 class Api::V1::RoadTripsController < ApplicationController
 	def create
-		road_trip = RoadTripsFacade.create_trip(params[:road_trip][:origin], params[:road_trip][:destination], params[:road_trip][:api_key]) ### remove api_key from arguments?
+		road_trip = RoadTripsFacade.create_trip(params[:road_trip][:origin], params[:road_trip][:destination])
+		render json: RoadTripsSerializer.new(road_trip)
 	end
 end
