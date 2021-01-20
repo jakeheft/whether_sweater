983bfcca970db8921e752789f1bc3451b5099367
Jake Heft <jakeheft@gmail.com>
2021-01-19 17:38:01 -0700

    Add half poro test for trip data

--- spec/poros/road_trip_spec.rb
+++ spec/poros/road_trip_spec.rb
@@ -2,6 +2,21 @@ require 'rails_helper'
 
 describe RoadTrip do
 	it 'exists with attributes' do
-		
+		trip_data = {
+			route: {
+				formattedTime: '09:13:50',
+				locations: [{
+					adminArea5: 'Denver',
+					adminArea3: 'CO'
+				},
+				{
+					adminArea5: 'Des Moines',
+					adminArea3: 'IA'
+				}
+			]
+			}
+		}
+		weather_data = {}
+		road_trip = RoadTrip.new(trip_data, weather_data)
 	end
 end
