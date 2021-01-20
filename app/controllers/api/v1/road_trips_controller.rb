169e7026ddd659412e25a71df16fda75f532744b
Jake Heft <jakeheft@gmail.com>
2021-01-19 21:42:32 -0700

    Add missing location helper method

--- app/controllers/api/v1/road_trips_controller.rb
+++ app/controllers/api/v1/road_trips_controller.rb
@@ -3,11 +3,15 @@ class Api::V1::RoadTripsController < ApplicationController
 		user = User.find_by(api_key: params[:api_key])
 		if user == nil
 			render json: ErrorSerializer.invalid_api_key, status: :unauthorized
-		elsif params[:origin] == '' || params[:destination] == ''
+		elsif missing_location?
 			render json: ErrorSerializer.empty_query, status: 400
 		else
 			road_trip = RoadTripsFacade.create_trip(params[:road_trip][:origin], params[:road_trip][:destination])
 			render json: RoadTripsSerializer.new(road_trip)
 		end
+	end
+
+	def missing_location?
+		params[:origin] == '' || params[:origin] == nil || params[:destination] == '' || params[:destination] == nil
 	end
 end
