7c4259472d7ff240287e180dbb7d09b63ac06560
Jake Heft <jakeheft@gmail.com>
2021-01-19 18:25:04 -0700

    Add more thorough testing for length of trip

--- spec/facades/road_trips_facade_spec.rb
+++ spec/facades/road_trips_facade_spec.rb
@@ -1,9 +1,18 @@
 require 'rails_helper'
 
 describe RoadTripsFacade do
-	it '.create_trip()' do
+	it '.create_trip(), shorter than 48 hours' do
 		origin = 'Denver,CO'
 		destination = 'Austin,TX'
+		api_key = 'Sdfiodso*DfSD89sdf'
+
+		road_trip = RoadTripsFacade.create_trip(origin, destination, api_key)
+		expect(road_trip).to be_a(RoadTrip)
+	end
+
+	it '.create_trip(), longer than 48 hours' do
+		origin = 'SanDiego,CA'
+		destination = 'Caribou,ME'
 		api_key = 'Sdfiodso*DfSD89sdf'
 
 		road_trip = RoadTripsFacade.create_trip(origin, destination, api_key)
